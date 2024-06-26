baab4cb660f6851094e19c1089b3c4ea3d36def3
Strashn <162972211+Strashn@users.noreply.github.com>
2024-06-26 20:31:17 +0300

    Add files via upload

---
+++ lesson_22/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// let someVar = 0;
+// ++someVar;
+// if (someVar) {
+//     console.log(someVar);
+// }
+
+
+// let addString = `Пункт №`
+// for (let i = 1; i < 11; ++i) {
+//     console.log(addString + [i])
+// }
+
+
+// function getQuotient(a, b) {
+//     const result = a / b
+//     if (isNaN(result) || result === Infinity)
+//         return (`Введіть коректні значення`)
+
+//     return `Результат ділення: ${result}`
+// }
+
+
+
+// console.log(getQuotient(8, 4))
+// console.log(getQuotient(8, 0))
+// console.log(getQuotient())
+// console.log(getQuotient(5, `John`))
+// console.log(getQuotient(-12, 3))
+
+
+// const tenArray = [`John`, 10, true, 10, 4]
+// tenArray.forEach((item, index) => {
+//     if (item === 10) {
+//         console.log(item, index);
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
