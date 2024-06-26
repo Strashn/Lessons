d8e30bfd3ed75a95508b97b6c485113899f2e28c
Strashn <162972211+Strashn@users.noreply.github.com>
2024-06-26 20:30:56 +0300

    Add files via upload

---
+++ lesson_21/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// const userName = 20
+// userName = "Іштван"
+// console.log(userName)
+
+// let numOne = 20
+// let numTwo = "20"
+// let numSumm = numOne + numTwo
+// console.log(numSumm)
